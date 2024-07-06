e91bc7a1765c0a9a515c10c0987233d6df4c7633
Ostrinka <148815239+Ostrinka@users.noreply.github.com>
2024-07-06 18:14:39 +0300

    generateContacts

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,5 +1,21 @@
 import { PATH_DB } from '../constants/contacts.js';
+import { createFakeContact } from '../utils/createFakeContact.js';
+import fs from 'node:fs/promises';
 
-const generateContacts = async (number) => {};
+const generateContacts = async (number) => {
+  try {
+    let data = await fs.readFile(PATH_DB, 'utf8');
+    let contacts = JSON.parse(data);
+
+    for (let i = 0; i < number; i++) {
+      contacts.push(createFakeContact());
+    }
+
+    await fs.writeFile(PATH_DB, JSON.stringify(contacts, undefined, 2));
+    console.log(`Successfully added ${number} new contacts.`);
+  } catch (error) {
+    console.error(error);
+  }
+};
 
 generateContacts(5);
